3b039989c03fe81415aa520816e001630013c4c7
Hanyu Gao <hanyugao@Hanyus-MacBook-Pro.local>
2020-09-04 20:35:51 -0400

    downloaded new file db.py

---
+++ db.py
@@ -0,0 +1,26 @@
+pistachio_pkl_path = '/home/ljn/lab_jensen/pistachio-db/script/db_dump'
+pistachio_txt_path = '/home/ljn/lab_jensen/pistachio-db/data/2019-11-18/extract'
+
+data_dir = './data/'
+stage0_dir = data_dir + 'stage0'
+stage1_dir = data_dir + 'stage1'
+
+data_fn = stage0_dir + '/' + 'data.json'
+
+class_name_json = [
+    'rxn_class_name_super0.json',
+    'rxn_class_name_super1.json',
+    'rxn_class_name.json',
+]
+
+encoder_json = [
+    'encoder0.json',
+    'encoder1.json',
+    'encoder2.json',
+]
+
+decoder_json = [
+    'decoder0.json',
+    'decoder1.json',
+    'decoder2.json',
+]
