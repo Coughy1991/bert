8ff815e33bec495334dfd78ab221771e9bce7cba
Hanyu Gao <hanyugao@Hanyus-MacBook-Pro.local>
2020-09-04 22:56:42 -0400

    added fast filter processor

--- run_classifier.py
+++ run_classifier.py
@@ -373,6 +373,39 @@ class ColaProcessor(DataProcessor):
           InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
     return examples
 
+class FastFilterProcessor(DataProcessor):
+  """Processor for the fast filter data set (processed version)."""
+  
+  def get_train_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_txt(os.path.join(data_dir, "train.txt")), "train")
+
+  def get_dev_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_txt(os.path.join(data_dir, "val.txt")), "dev")
+
+  def get_test_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_txt(os.path.join(data_dir, "test.txt")), "test")
+
+  def get_labels(self):
+    """See base class."""
+    return ['0','1']
+
+  def _create_examples(self, lines, set_type):
+    """Creates examples for the training and dev sets."""
+    examples = []
+    for (i, line) in enumerate(lines):
+      guid = "%s-%s" % (set_type, i)
+      line_split = line.split(" ")
+      text_a = " ".join(line_split[1:])
+      label = line_split[0]
+      examples.append(
+          InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+    return examples
 
 def convert_single_example(ex_index, example, label_list, max_seq_length,
                            tokenizer):
